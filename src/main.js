49d0df4a469821047b3052c48d0ab4dbbcfc1453
Hans Chiu <chiuhans111@gmail.com>
2025-03-01 11:30:28 +0800

    [add] MS Clarity

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import Clarity from '@microsoft/clarity';
+const projectId = "qh7qxpa2do"
+Clarity.init(projectId);
+
 import './style.css'
 import {regl} from './render'
 import {update} from './interactive'
